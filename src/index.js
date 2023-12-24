f22d641d81e28b30812004f7ec1c5c5ca15299f7
Deepak jaat <deepakjhajhria4657@gmail.com>
2023-12-24 17:22:36 +0530

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-
+window.addEventListener("beforeunload",(event)=>{
+  event.preventDefault();
+  event.returnValue = "";
+});
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
